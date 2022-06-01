babb48de80fc8b1e6004fa1885ba5ab20aab4f01
Nitish Sharma <43414719+Nitish-1601@users.noreply.github.com>
2022-06-01 13:41:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,29 @@ import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
-
-const App = () => {
-    if(!localStorage.getItem('username')) return <LoginForm />
-
-    return (
-          <ChatEngine
-            height = "100vh"
-            projectID = "d82ed3dd-ae92-4ca6-8f0a-cd21e251b538"
-            userName = {localStorage.getItem('username')}
-            userSecret = {localStorage.getItem('password')}
-            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
-            />
-    );
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='nitish1601'
+			userSecret='N.S.1999'
+			projectID='d82ed3dd-ae92-4ca6-8f0a-cd21e251b538'
+		/>
+	);
 }
 
-export default App;
+// const App = () => {
+//     if(!localStorage.getItem('username')) return <LoginForm />
+
+//     return (
+//           <ChatEngine
+//             height = "100vh"
+//             projectID = "d82ed3dd-ae92-4ca6-8f0a-cd21e251b538"
+//             userName = {localStorage.getItem('username')}
+//             userSecret = {localStorage.getItem('password')}
+//             renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+//             />
+//     );
+// }
+
+// export default App;
